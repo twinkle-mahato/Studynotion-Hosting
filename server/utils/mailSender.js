912008e39577bd8fcdac9137f210647803808c14
twinkle-mahato <twinklemahato8670@gmail.com>
2025-10-15 23:46:47 +0530

    Update secure false to true

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -6,7 +6,7 @@ const mailSender = async (email, title, body) => {
 
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
