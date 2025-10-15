ff573bb5335b4afa6cd5ffdabf1316f7a1feb015
twinkle-mahato <twinklemahato8670@gmail.com>
2025-10-15 23:56:15 +0530

    Update file

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -6,14 +6,14 @@ const mailSender = async (email, title, body) => {
 
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      secure: true,
+      // secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
+      // tls: {
+      //   rejectUnauthorized: false,
+      // },
     });
 
     let info = await transporter.sendMail({
