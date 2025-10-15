e4764df931cfe12484ba77cc63e2c14c7f3fd82e
twinkle-mahato <twinklemahato8670@gmail.com>
2025-10-16 00:18:47 +0530

    Add Port

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -6,6 +6,7 @@ const mailSender = async (email, title, body) => {
 
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: 587,
       // secure: true,
       auth: {
         user: process.env.MAIL_USER,
